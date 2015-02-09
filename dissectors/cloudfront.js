c3980e64e588e08e79101eec538aad9cc6d499f1
Jujhar Singh <jujhar@jujhar.com>
2015-02-09 15:51:44 +0000

    yay it works, thank god for docker

--- dissectors/cloudfront.js
+++ dissectors/cloudfront.js
@@ -1,12 +1,12 @@
 /**
- * dissector for amazon cloudfront v1.0 acess log files
- * @author jujhars13
+ * dissector for Amazon CloudFront v1.0 access log files
+ * @author jujhars13 2015-02-09
  * @link http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/AccessLogs.html#LogFileFormat
  */
-//Fields: date time x-edge-location sc-bytes c-ip cs-method cs(Host) cs-uri-stem sc-status cs(Referer) cs(User-Agent) cs-uri-query cs(Cookie) x-edge-result-type x-edge-request-id x-host-header cs-protocol cs-bytes time-taken
+//Fields: date time x-edge-location sc-bytes c-ip cs-method cs(Host) cs-uri-stem sc-status cs(Referrer) cs(User-Agent) cs-uri-query cs(Cookie) x-edge-result-type x-edge-request-id x-host-header cs-protocol cs-bytes time-taken
 //eg
 //2015-01-28	18:41:47	LHR5	4082	195.214.31.26	GET	e3lkfdfzknjs2t.cloudfront.net	/buto/europe/videos/Ft5xk/1/BFSdb/BFSdb_thumbnail_large_50.jpg	200	-	curl/7.35.0	-	-	Hit	gq6qEMK_nqhjDM4mXDZWGgoh_xxOClMJIwlGbGJt9TmZSIjOJmxLWg==	e3lkfdfzknjs2t.cloudfront.net	https	154	0.001
-var regex = /(\S+?) (\S+?) (\S+? \+\S+?\]) (\S+?) (\S+?) (\S+?) (\S+?) (\S+?) "(\S+?) (\S+?) (\S+?)" (\S+?) (\S+?) (\S+?) (\S+?) (\S+?) (\S+?) (\S+?) "(.*?)" (\S+?)/
+var regex = /^(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+\S+)$/
 
 var map = {
     1: 'date',
@@ -14,7 +14,7 @@ var map = {
     3: 'x_edge_location',
     4: 'bytes_sent',
     5: 'client_ip',
-    6: 'http_method',
+    6: 'verb',
     7: 'server_host',
     8: 'uri',
     9: 'status_code',
--- test/cloudfrontTest.js
+++ test/cloudfrontTest.js
@@ -10,11 +10,12 @@ var dissector = require('../index').dissectors['cloudfront'];
 
 //test data
 var valid_log_lines = {
-    1: '89db1f87ef900402d4ec4678ec2d46ef9c503f28438eeaeb3a8409ee06106660 buto-s3-cdn-origin [07/May/2013:19:01:44 +0000] 91.229.127.20 - 0B7A7459271E19B0 REST.GET.OBJECT live/videos/Bbgwg/1/2pgGq/1byte.txt "GET /butotv/live/1/6tYhhg1/6tYhhg1_frame_custom_0000.jpg HTTP/1.1" 200 - 1 1 1 12 "-" "curl/7.29.0" -',
-    100: '89db1f87ef900402d4ec4678ec2d46ef9c503f28438eeaeb3a8409ee06106660 buto-s3-cdn-origin [07/May/2013:19:01:44 +0000] 91.229.127.20 - 0B7A7459271E19B0 REST.GET.OBJECT live/videos/Bbgwg/1/2pgGq/1byte.txt "GET /butotv/live/1/6tYhhg1/6tYhhg1_frame_custom_0000.jpg HTTP/1.1" 200 - 100 1 100 12 "-" "curl/7.29.0" -',
-    1000: '89db1f87ef900402d4ec4678ec2d46ef9c503f28438eeaeb3a8409ee06106660 buto-s3-cdn-origin [07/May/2013:19:01:44 +0000] 91.229.127.20 - 0B7A7459271E19B0 REST.GET.OBJECT live/videos/Bbgwg/1/2pgGq/1byte.txt "GET /butotv/live/1/6tYhhg1/6tYhhg1_frame_custom_0000.jpg HTTP/1.1" 200 - 1000 1 1000 12 "-" "curl/7.29.0" -',
-    10000: '89db1f87ef900402d4ec4678ec2d46ef9c503f28438eeaeb3a8409ee06106660 buto-s3-cdn-origin [07/May/2013:19:01:44 +0000] 91.229.127.20 - 0B7A7459271E19B0 REST.GET.OBJECT live/videos/Bbgwg/1/2pgGq/1byte.txt "GET /butotv/live/1/6tYhhg1/6tYhhg1_frame_custom_0000.jpg HTTP/1.1" 200 - 10000 1 1000 12 "-" "curl/7.29.0" -',
-    100000: '89db1f87ef900402d4ec4678ec2d46ef9c503f28438eeaeb3a8409ee06106660 buto-s3-cdn-origin [07/May/2013:19:01:44 +0000] 91.229.127.20 - 0B7A7459271E19B0 REST.GET.OBJECT live/videos/Bbgwg/1/2pgGq/1byte.txt "GET /butotv/live/1/6tYhhg1/6tYhhg1_frame_custom_0000.jpg HTTP/1.1" 200 - 100000 1 1000 12 "-" "curl/7.29.0" -'
+    1: '2015-01-28	18:41:47	LHR5	1	195.214.31.26	GET	e3lkfdfzknjs2t.cloudfront.net	/buto/europe/videos/Ft5xk/1/BFSdb/BFSdb_thumbnail_large_50.jpg	200	-	curl/7.35.0	-	-	Hit	gq6qEMK_nqhjDM4mXDZWGgoh_xxOClMJIwlGbGJt9TmZSIjOJmxLWg==	e3lkfdfzknjs2t.cloudfront.net	https	154	0.001',
+    100: '2015-01-28	18:41:47	LHR5	100	195.214.31.26	GET	e3lkfdfzknjs2t.cloudfront.net	/buto/europe/videos/Ft5xk/1/BFSdb/BFSdb_thumbnail_large_50.jpg	200	-	curl/7.35.0	-	-	Hit	gq6qEMK_nqhjDM4mXDZWGgoh_xxOClMJIwlGbGJt9TmZSIjOJmxLWg==	e3lkfdfzknjs2t.cloudfront.net	https	154	0.001',
+    4082: '2015-01-28	18:41:47	LHR5	4082	195.214.31.26	GET	e3lkfdfzknjs2t.cloudfront.net	/buto/europe/videos/Ft5xk/1/BFSdb/BFSdb_thumbnail_large_50.jpg	200	-	curl/7.35.0	-	-	Hit	gq6qEMK_nqhjDM4mXDZWGgoh_xxOClMJIwlGbGJt9TmZSIjOJmxLWg==	e3lkfdfzknjs2t.cloudfront.net	https	154	0.001',
+    1000: '2015-01-28	18:41:47	LHR5	1000	195.214.31.26	GET	e3lkfdfzknjs2t.cloudfront.net	/buto/europe/videos/Ft5xk/1/BFSdb/BFSdb_thumbnail_large_50.jpg	200	-	curl/7.35.0	-	-	Hit	gq6qEMK_nqhjDM4mXDZWGgoh_xxOClMJIwlGbGJt9TmZSIjOJmxLWg==	e3lkfdfzknjs2t.cloudfront.net	https	154	0.001',
+    10000: '2015-01-28	18:41:47	LHR5	10000	195.214.31.26	GET	e3lkfdfzknjs2t.cloudfront.net	/buto/europe/videos/Ft5xk/1/BFSdb/BFSdb_thumbnail_large_50.jpg	200	-	curl/7.35.0	-	-	Hit	gq6qEMK_nqhjDM4mXDZWGgoh_xxOClMJIwlGbGJt9TmZSIjOJmxLWg==	e3lkfdfzknjs2t.cloudfront.net	https	154	0.001',
+    100000: '2015-01-28	18:41:47	LHR5	100000	195.214.31.26	GET	e3lkfdfzknjs2t.cloudfront.net	/buto/europe/videos/Ft5xk/1/BFSdb/BFSdb_thumbnail_large_50.jpg	200	-	curl/7.35.0	-	-	Hit	gq6qEMK_nqhjDM4mXDZWGgoh_xxOClMJIwlGbGJt9TmZSIjOJmxLWg==	e3lkfdfzknjs2t.cloudfront.net	https	154	0.001'
 };
 
 describe('s3', function () {
@@ -26,15 +27,16 @@ describe('s3', function () {
             //foreach over our test data array and test
             _(valid_log_lines).forEach(function (line, key) {
                 var data_should_be = {
-                    timestamp: '[07/May/2013:19:01:44 +0000]',
+                    date: '2015-01-28',
+                    time: '18:41:47',
                     verb: 'GET',
-                    uri: '/butotv/live/1/6tYhhg1/6tYhhg1_frame_custom_0000.jpg',
-                    protocol: 'HTTP/1.1',
-                    client_ip: '91.229.127.20',
+                    uri: '/buto/europe/videos/Ft5xk/1/BFSdb/BFSdb_thumbnail_large_50.jpg',
+                    protocol: 'https',
+                    client_ip: '195.214.31.26',
                     bytes_sent: key,
-                    'status_code': '200',
-                    'user-agent': 'curl/7.29.0',
-                    type: 's3'
+                    status_code: '200',
+                    user_agent: 'curl/7.35.0',
+                    type: 'cloudfront'
                 };
                 //parse the record
 
@@ -42,6 +44,7 @@ describe('s3', function () {
 
                 //iterate over our data array and compare values with what's returned
                 _(data_should_be).forEach(function (value, data_key) {
+
                     (value).should.equal(data[data_key]);
                 });
 
@@ -49,29 +52,29 @@ describe('s3', function () {
         });
 
         it("should dissect all values from logs without failure", function () {
-            var testData = '04db613bd000d07badc32d16138efc3eba3e96c6c3ca19365997ba468a6ac850 test.repo.io [10/Apr/2013:18:27:38 +0000] 54.225.77.116 arn:aws:iam::581309990414:user/test-registry 4981BCEDE3870133 REST.PUT.OBJECT incompatible-version/incompatible-version-1.0.0.zip "PUT /test.repo.io/incompatible-version/incompatible-version-1.0.0.zip HTTP/1.1" 200 - - 504 89 32 "-" "aws-sdk-nodejs/v0.9.7-pre.8 linux/v0.8.22" -';
+            var testData = '2015-01-28	18:50:46	LHR5	4082	191.224.31.66	GET	f3lkew8zqnjt1t.cloudfront.net	/buto/asia/videos/B9ixk/1/FFSbb/FFSbb_thumbnail_large_50.jpg	200	jujhar.com	curl/7.35.0	-	-	Hit	CFlXeIhIB_JVf6IuX3FDDSqNt0l3WIqmZBjKvwpJbYxaH1BXAwpUiA==	f3lkew8zqnjt1t.cloudfront.net	https	154	0.001';
 
             var data_should_be = {
-                owner: '04db613bd000d07badc32d16138efc3eba3e96c6c3ca19365997ba468a6ac850',
-                bucket: 'test.repo.io',
-                timestamp: '[10/Apr/2013:18:27:38 +0000]',
-                client_ip: '54.225.77.116',
-                requester: 'arn:aws:iam::581309990414:user/test-registry',
-                request_id: '4981BCEDE3870133',
-                operation: 'REST.PUT.OBJECT',
-                key: 'incompatible-version/incompatible-version-1.0.0.zip',
-                verb: 'PUT',
-                uri: '/test.repo.io/incompatible-version/incompatible-version-1.0.0.zip',
-                protocol: 'HTTP/1.1',
-                'status_code': '200',
-                'error_code': '-',
-                'bytes_sent': '-',
-                'object_size': '504',
-                'time_total': '89',
-                'time_turn_around': '32',
-                'referrer': '"-"',
-                'user-agent': 'aws-sdk-nodejs/v0.9.7-pre.8 linux/v0.8.22',
-                type: 's3'
+                date: '2015-01-28',
+                time: '18:50:46',
+                x_edge_location: 'LHR5',
+                bytes_sent: '4082',
+                client_ip: '191.224.31.66',
+                verb: 'GET',
+                server_host: 'f3lkew8zqnjt1t.cloudfront.net',
+                uri: '/buto/asia/videos/B9ixk/1/FFSbb/FFSbb_thumbnail_large_50.jpg',
+                status_code: '200',
+                referrer: 'jujhar.com',
+                user_agent: 'curl/7.35.0',
+                uri_query: '-',
+                cookie: '-',
+                x_edge_result_type: 'Hit',
+                x_edge_request_id: 'CFlXeIhIB_JVf6IuX3FDDSqNt0l3WIqmZBjKvwpJbYxaH1BXAwpUiA==',
+                x_host_headers: 'f3lkew8zqnjt1t.cloudfront.net',
+                protocol: 'https',
+                bytes_recieved: '154',
+                time_taken: '0.001',
+                type: 'cloudfront'
             };
 
             //parse the record
